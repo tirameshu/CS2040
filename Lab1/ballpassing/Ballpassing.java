b53581a15b056f0d9f6f3b341951bb1ceb796d2a
Mandy <shunflower320@gmail.com>
2019-01-27 22:42:36 +0800

    finished ballpassing

--- Lab1/ballpassing/Ballpassing.java
+++ Lab1/ballpassing/Ballpassing.java
@@ -1,6 +1,6 @@
 /**
- * Name         :
- * Matric. No   :
+ * Name         : Wang Xinman
+ * Matric. No   : A0180257E
  * PLab Acct.   :
  */
 
@@ -8,7 +8,37 @@ import java.util.*;
 
 public class Ballpassing {
 	private void run() {
-		//implement your "main" method here
+        List<String> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            students.add(sc.next());
+        }
+
+        long q = sc.nextLong();
+        int index = 0;
+
+        for (int j = 0; j < q; j++) {
+            String event = sc.next();
+
+            if (event.equals("NEXT")) {
+                index++;
+            } else if (event.equals("JOIN")) {
+                String newStudent = sc.next();
+                index++;
+                if (index <= students.size()) {
+                    students.add(index, newStudent);
+                } else {
+                    students.add(newStudent);
+                }
+            } else {
+                students.remove(index);
+            }
+
+            index = index % students.size();
+            System.out.println(students.get(index));
+        }
 	}
 
 	public static void main(String[] args) {
