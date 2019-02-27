321788fc6622d90ffa7044583e1c88b12f303527
Mandy <shunflower320@gmail.com>
2019-02-27 09:15:01 +0800

    gcd

---
+++ Practices/GCD.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class GCD {
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a%b);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(a, b));
+    }
+}
