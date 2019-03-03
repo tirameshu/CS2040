9cfb08e30556fac0fa4aff9c3d8d1bd2b03319d4
Mandy <shunflower320@gmail.com>
2019-03-03 23:15:13 +0800

    so close

--- Lab/2/Generate/Generate.java
+++ Lab/2/Generate/Generate.java
@@ -7,10 +7,56 @@
 import java.util.*;
 
 public class Generate {
+    private static ArrayList<String> perms = new ArrayList<>();
+    private static ArrayList<String> powerSet = new ArrayList<>();
+    private ArrayList<String> slotAndSubseq(char firstLetter, ArrayList<String> lst) {
+        ArrayList<String> newList = new ArrayList<>();
+        for (String s: lst) {
+            for (int i = 0; i <= s.length(); i++) {
+                String string = new StringBuilder(s).insert(i, firstLetter).toString();
+                char[] arr = string.toCharArray();
+                Arrays.sort(arr);
+                String sortedString = new String(arr);
+                System.out.println("sorted: " + sortedString);
+                if (i == 0 && !powerSet.contains(sortedString)) {//adds to the front of the last letter in the string
+                    powerSet.add(string);
+                }
+                if (!newList.contains(string)) {
+                    newList.add(string);
+                }
+            }
+        }
+        
+        return newList;
+    }
+
     private ArrayList<String> permutate(String str) {
+        if (str.length() == 0) {
+            ArrayList<String> lst = new ArrayList<>();
+            lst.add(str);
+            return lst;
+        }
         char firstLetter = str.charAt(0);
         String substring = new StringBuilder(str).substring(1);
-        return slot(firstLetter, substring);
+        return slotAndSubseq(firstLetter, permutate(substring));
+    }
+
+    private void subSeq(String string) {
+        StringBuilder sb;
+        for (int j = 0; j < string.length(); j++) {
+            sb = new StringBuilder(string);
+            String letter = sb.substring(j, j+1).toString(); //add the individual letters
+            if (!powerSet.contains(letter)) {
+                powerSet.add(letter);
+            }
+            for (int k = j + 1; k < string.length(); k++) {
+                sb = new StringBuilder(letter); //ensures that all possible pairs are made
+                String toAdd = sb.append(string.charAt(k)).toString();
+                if (!powerSet.contains(toAdd)) {
+                    powerSet.add(toAdd);
+                }
+            }
+        }
     }
 
 	private void run() {
@@ -18,7 +64,16 @@ public class Generate {
         Scanner sc = new Scanner(System.in);
 
         String w = sc.next();
-        int length = w.length();
+        perms = permutate(w);
+        Collections.sort(perms);
+        for (String res: perms) {
+            System.out.println(res);
+        }
+        subSeq(w);
+        Collections.sort(powerSet);
+        for (String p: powerSet) {
+            System.out.println(p);
+        }
 	}
 
 	public static void main(String[] args) {
