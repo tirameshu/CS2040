efb2260125c3f63dc375d17ab120b665d43a070f
Mandy Wang <shunflower320@gmail.com>
2019-04-04 22:31:20 +0800

    trying sil 1b

---
+++ Sit-in/1b/kcats/ex3/Kcats.java
@@ -0,0 +1,18 @@
+/**
+ * Name         :
+ * Matric. No   :
+ * PLab Acct.   :
+ */
+
+import java.util.*;
+
+public class Kcats {
+	private void run() {
+		//implement your "main" method here
+	}
+
+	public static void main(String[] args) {
+		Kcats newKcats = new Kcats();
+		newKcats.run();
+	}
+}
