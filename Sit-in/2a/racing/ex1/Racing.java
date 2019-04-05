5de6313c1a53e008f5d7415a7648082f0daa4a0c
Mandy Wang <shunflower320@gmail.com>
2019-04-05 12:05:19 +0800

    finished 2a

--- Sit-in/2a/racing/ex1/Racing.java
+++ Sit-in/2a/racing/ex1/Racing.java
@@ -7,8 +7,59 @@
 import java.util.*;
 
 public class Racing {
+	List<List<Integer>> result = new ArrayList<>(); // to be populated with the permutations
+	int n;
+	long d;
+
+	private void permute(List<Integer> start, List<Integer> end) {
+		if (end.size() == 0) {
+			result.add(start);
+		} else {
+			for (int k = 0; k < end.size(); k++) {
+				int element = end.get(k);
+				List<Integer> newStart = new ArrayList<>();
+				for (int e: start) {
+					newStart.add(e);
+				}
+				newStart.add(element);
+				List<Integer> newEnd = new ArrayList<>();
+				for (int e2: end) {
+					newEnd.add(e2);
+				}
+				newEnd.remove(k);
+				permute(newStart, newEnd);
+			}
+		}
+	}
+
+	private int count() {
+		int counter = 0;
+		for (List<Integer> perm: result) {
+			int difficulty = 0;
+			for (int j = 0; j < n-1; j++) {
+				difficulty += Math.pow(perm.get(j+1) - perm.get(j), 2);
+			}
+
+			if (difficulty <= d) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
 	private void run() {
 		//implement your "main" method here
+		Scanner sc = new Scanner(System.in);
+		List<Integer> original = new ArrayList<>();
+		n = sc.nextInt();
+		d = sc.nextLong();
+
+		for (int i = 0; i < n; i++) {
+			original.add(sc.nextInt());
+		}
+
+		permute(new ArrayList<Integer>(), original);
+		System.out.println(count());
 	}
 
 	public static void main(String[] args) {
--- Sit-in/2b/fractions/ex2/Fractions.java
+++ Sit-in/2b/fractions/ex2/Fractions.java
@@ -9,10 +9,62 @@ import java.util.*;
 public class Fractions {
 	private void run() {
 		//implement your "main" method here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		PriorityQueue<MixedFrac> queue = new PriorityQueue<>();
+
+		for (int i = 0; i < n; i++) {
+			MixedFrac frac = new MixedFrac(sc.nextInt(), sc.nextInt(), sc.nextInt());
+			queue.add(frac);
+		}
+
+		while (!queue.isEmpty()) {
+			System.out.println(queue.poll());
+		}
 	}
 
 	public static void main(String[] args) {
 		Fractions newFractions = new Fractions();
 		newFractions.run();
+	}
+}
+
+class MixedFrac implements Comparable<MixedFrac> {
+	int a;
+	int b;
+	int c;
+	long numerator;
+	int denominator;
+
+	public MixedFrac(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.numerator = a*c+b;
+		this.denominator = c;
+	}
+
+	public int compareTo(MixedFrac other) {
+		long lhs = this.numerator * other.denominator;
+		long rhs = this.denominator * other.numerator;
+		if (lhs < rhs) {
+			return -1;
+		} else if (lhs == rhs) {
+			if (this.a == other.a) {
+				if (this.b == other.b) {
+					return this.c - other.c;
+				} else {
+					return this.b - (other.b);
+				}
+			} else {
+				return this.a - other.a;
+			}
+		} else {
+			return 1;
+		}
+	}
+
+	public String toString() {
+		return (this.a + " " + this.b + " " + this.c);
 	}
 }
