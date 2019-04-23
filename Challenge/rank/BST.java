419450de659fdae99a62c381abb6fef5bc1080d3
Mandy Wang <shunflower320@gmail.com>
2019-04-23 14:45:39 +0800

    tried to improve time for rank

---
+++ Challenge/rank/BST.java
@@ -0,0 +1,329 @@
+import java.util.*;
+
+// barebones implementation of BST
+
+// Every vertex in this BST is a Java Class
+class BSTVertex {
+    BSTVertex(int v) { key = v; parent = left = right = null; height = 0; size = 1; rank = -1; }
+    // all these attributes remain public to slightly simplify the code
+    public BSTVertex parent, left, right;
+    public int key;
+    public int height; // will be used in lecture on AVL
+    public int size;
+	public int rank;
+}
+
+// This is just a sample implementation
+// There are other ways to implement BST concepts...
+public class BST {
+    public BSTVertex root;
+    public boolean rotated;
+
+    public BST() { root = null; }
+
+    // public method called to search for a value v. 
+    // Return v if it is found in the BST otherwise return -1.
+    // Here the assumption is that -1 is never a valid key value.
+    public int search(int v) {
+        BSTVertex res = search(root, v);
+        return res == null ? -1 : res.key;
+    }
+
+    // helper method to perform search
+    private BSTVertex search(BSTVertex T, int v) {
+        if (T == null)  return null;                     // not found
+        else if (T.key == v) return T;                        // found
+        else if (T.key < v)  return search(T.right, v);       // search to the right
+        else                 return search(T.left, v);        // search to the left
+    }
+
+    // public method called to find Minimum key value in BST
+    public int findMin() { return findMin(root); }
+
+    // helper method to perform findMin
+    // Question: What happens if BST is empty?
+    private int findMin(BSTVertex T) {
+        if (T.left == null) return T.key;                    // this is the min
+        else                return findMin(T.left);           // go to the left
+    }
+
+    // public method called to find Maximum key value in BST
+    public int findMax() { return findMax(root); }
+
+    // helper method to perform findMax
+    // Question: Again, what happens if BST is empty?
+    private int findMax(BSTVertex T) {
+        if (T.right == null) return T.key;                   // this is the max
+        else                 return findMax(T.right);        // go to the right
+    }
+
+    // public method to find successor to given value v in BST.
+    public int successor(int v) { 
+        BSTVertex vPos = search(root, v);
+        return vPos == null ? -1 : successor(vPos);
+    }
+
+    // helper recursive method to find successor to for a given vertex T in BST
+    private int successor(BSTVertex T) {
+        if (T.right != null)                       // this subtree has right subtree
+            return findMin(T.right);  // the successor is the minimum of right subtree
+        else {
+            BSTVertex par = T.parent;
+            BSTVertex cur = T;
+            // if par(ent) is not root and cur(rent) is its right children
+            while ((par != null) && (cur == par.right)) {
+                cur = par;                                         // continue moving up
+                par = cur.parent;
+            }
+            return par == null ? -1 : par.key;           // this is the successor of T
+        }
+    }
+
+    // public method to find predecessor to given value v in BST
+    public int predecessor(int v) { 
+        BSTVertex vPos = search(root, v);
+        return vPos == null ? -1 : predecessor(vPos);
+    }
+
+    // helper recursive method to find predecessor to for a given vertex T in BST
+    private int predecessor(BSTVertex T) {
+        if (T.left != null)                         // this subtree has left subtree
+            return findMax(T.left);  // the predecessor is the maximum of left subtree
+        else {
+            BSTVertex par = T.parent;
+            BSTVertex cur = T;
+            // if par(ent) is not root and cur(rent) is its left children
+            while ((par != null) && (cur == par.left)) { 
+                cur = par;                                         // continue moving up
+                par = cur.parent;
+            }
+            return par == null ? -1 : par.key;           // this is the successor of T
+        }
+    }
+
+    // public method called to perform inorder traversal
+    public void inorder() { 
+        inorder(root);
+        System.out.println();
+    }
+
+    // helper method to perform inorder traversal
+    private void inorder(BSTVertex T) {
+        if (T == null) return;
+        inorder(T.left);                               // recursively go to the left
+        System.out.printf(" %d", T.key);                      // visit this BST node
+        inorder(T.right);                             // recursively go to the right
+    }
+
+    // public method called to insert a new key with value v into BST
+    public void insert(int v) { root = insert(root, v); }
+
+    public int balanceFactor(BSTVertex T) {
+        int heightL, heightR;
+        if (T.left == null) {
+            heightL = -1;
+        } else {
+            heightL = T.left.height; // changed
+        }
+
+        if (T.right == null) {
+            heightR = -1;
+        } else {
+            heightR = T.right.height; // changed
+        }
+
+        T.height = Math.max(heightL, heightR) + 1;
+
+        return heightL - heightR;
+    }
+
+    // helper recursive method to perform insertion of new vertex into BST
+    private BSTVertex insert(BSTVertex T, int v) {
+        if (T == null) return new BSTVertex(v);          // insertion point is found
+
+        if (T.key < v) {                                      // search to the right
+            T.right = insert(T.right, v);
+            T.right.parent = T;
+        }
+        else {                                                 // search to the left
+            T.left = insert(T.left, v);
+            T.left.parent = T;
+        }
+
+        if (rotated) {
+            T.size++;
+            if (T == this.root) rotated = false;
+            return T;
+        }
+
+        int bf = balanceFactor(T);
+
+        switch (bf) {
+            case 2:
+                rotated = true;
+                int bfL = balanceFactor(T.left);
+                if (bfL >= 0 && bfL <= 1) {
+                    BSTVertex vertex = rightRotate(T);
+                    return vertex;
+                } else if (bfL == -1) {
+                    T.left = leftRotate(T.left);
+                    BSTVertex vertex = rightRotate(T);
+                    return vertex;
+                } else {
+                    // System.out.println("something is wrong! bfL = " + bfL);
+                    break;
+                }
+
+            case -2:
+                rotated = true;
+                int bfR = balanceFactor(T.right);
+                if (bfR <= 0 && bfR >= -1) {
+                    BSTVertex vertex = leftRotate(T);
+                    return vertex;
+                } else if (bfR == 1) {
+                    T.right = rightRotate(T.right);
+                    BSTVertex vertex = leftRotate(T);
+                    return vertex;
+                } else {
+                    // System.out.println("something is wrong! bfR = " + bfR);
+                    break;
+                }
+        }
+
+        // does not require rotation
+        T.size++;
+
+        //resize(T);
+        //System.out.println("\nKey: " + T.key + " size: " + T.size + "\n");
+
+        return T;                                          // return the updated BST
+    }  
+
+    public BSTVertex rightRotate(BSTVertex T) {
+        BSTVertex leftOfT = T.left;
+        BSTVertex parentOfT = T.parent;
+
+        leftOfT.parent = parentOfT;
+        T.left = leftOfT.right;
+        if (leftOfT.right != null) leftOfT.right.parent = T;
+        T.parent = leftOfT;
+        leftOfT.right = T;
+
+        // after rotation
+        // updating size of T
+        if (T.right != null) {
+            if (T.left != null) T.size = T.left.size + T.right.size + 1;
+            else T.size = T.right.size + 1;
+        } else {
+            if (T.left != null) T.size = T.left.size + 1;
+            else T.size = 1;
+        }
+
+        // updating size of T's new parent
+        if (T.parent.left != null) T.parent.size = T.parent.left.size + T.size + 1;
+        else T.parent.size = T.size + 1;
+
+        return leftOfT;
+    }
+
+    public BSTVertex leftRotate(BSTVertex T) {
+        BSTVertex rightOfT = T.right;
+
+        rightOfT.parent = T.parent;
+        T.parent = rightOfT;
+        T.right = rightOfT.left;
+        if (rightOfT.left != null) rightOfT.left.parent = T;
+        rightOfT.left = T;
+
+        // after rotation
+        // updating size of T
+        if (T.right != null) {
+            if (T.left != null) T.size = T.left.size + T.right.size + 1;
+            else T.size = T.right.size + 1;
+        } else {
+            if (T.left != null) T.size = T.left.size + 1;
+            else T.size = 1;
+        }
+
+        // updating size of T's new parent
+        if (T.parent.right != null) T.parent.size = T.size + T.parent.right.size + 1;
+        else T.parent.size = T.size + 1;
+
+        return rightOfT;
+    }
+
+    // public method to get rank of vertex containing key with value v from BSt
+    public int getRank(int v) { return rank(root, v); }
+
+    // helper recursive method to find rank
+    private int rank(BSTVertex T, int v) {
+        int TLeftSize;
+        if (T.left == null) TLeftSize = 0; 
+        else TLeftSize = T.left.size;
+
+        if (T.key == v) {
+			T.rank = TLeftSize + 1;
+			return T.rank;
+		} else if (T.key > v) {
+			/*
+			if (T.left.rank == -1) {
+				T.left.rank = rank(T.left, T.left);
+			}
+			*/
+			if (T.left.rank != -1) {
+				T.rank = T.left.rank + 1;
+			}
+			return rank(T.left, v);
+		} else {
+			/*
+			if (T.right.rank == -1) {
+				T.right.rank = rank(T.right, T.right);
+			}
+			*/
+			if (T.right.rank != -1) {
+				T.rank = T.right.rank - 1;
+			}
+			return TLeftSize + 1 + rank(T.right, v);
+		}
+    }
+
+    // public method to delete a vertex containing key with value v from BST
+    public void delete(int v) { root = delete(root, v); }
+
+    // helper recursive method to perform deletion 
+    private BSTVertex delete(BSTVertex T, int v) {
+        if (T == null)  return T;              // cannot find the item to be deleted
+
+        if (T.key < v)                                    // search to the right
+            T.right = delete(T.right, v);
+        else if (T.key > v)                               // search to the left
+            T.left = delete(T.left, v);
+        else {                                            // this is the node to be deleted
+            if (T.left == null && T.right == null)                   // this is a leaf
+                T = null;                                      // simply erase this node
+            else if (T.left == null && T.right != null) {   // only one child at right        
+                T.right.parent = T.parent;
+                T = T.right;                                                 // bypass T        
+            }
+            else if (T.left != null && T.right == null) {    // only one child at left        
+                T.left.parent = T.parent;
+                T = T.left;                                                  // bypass T        
+            }
+            else {                                 // has two children, find successor
+                int successorV = successor(v);
+                T.key = successorV;         // replace this key with the successor's key
+                T.right = delete(T.right, successorV);      // delete the old successorV
+            }
+        }
+
+        return T;                                          // return the updated BST
+    }
+
+    // will be used in AVL lecture
+    private int getHeight(BSTVertex T) {
+        if (T == null) return -1;
+        else return Math.max(getHeight(T.left), getHeight(T.right)) + 1;
+    }
+
+    public int getHeight() { return getHeight(root); }
+}
